28e5638b13abf8f15dfbfb68493d3d2a2856e8c2
kaue <51096379+vini03@users.noreply.github.com>
2021-05-13 20:39:14 -0300

    Add files via upload

---
+++ lib/common/custom_drawer/drawer_title.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:teste2/models/page_manager.dart';
+import 'package:provider/provider.dart';
+
+class DrawerTitle extends StatelessWidget {
+  DrawerTitle({this.iconData, this.title,this.page});
+
+  final IconData iconData;
+  final String title;
+  final int page;
+  @override
+  Widget build(BuildContext context) {
+    final int curPage = context.watch<PageManager>().page;
+    return InkWell(
+      onTap: (){
+        context.read<PageManager>().setPage(page);
+      },
+      child: SizedBox(
+        height: 60,
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: Icon(
+                iconData,
+                size: 32,
+                color:  curPage == page ? Colors.red : Colors.grey[700],
+              ),
+            ),
+            Text(
+              title,
+              style: TextStyle(
+                  fontSize: 16,
+                  color:  curPage == page ? Colors.red : Colors.grey[700]
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
